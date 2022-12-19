0db18171593fb133ea77da18e023616f1a7936e2
LonGgGgGgGg <101206535+LonGgGgGgGgCN@users.noreply.github.com>
2022-12-19 18:10:07 +0800

    主要功能代码

--- Main.py
+++ Main.py
@@ -1,14 +1,13 @@
 # -*- coding: UTF-8 -*-
 # https://github.com/LonGgGgGgGgCN/SteamAccountSwitcher
 import steamid_converter as Converter
-from time import strftime, localtime, sleep
+from time import strftime, localtime
 from PyQt5.QtWidgets import QApplication, QWidget, QMessageBox
 from PyQt5.uic import loadUi
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 from sys import argv
 from os import system, startfile
-# import winreg
 from winreg import OpenKey, KEY_ALL_ACCESS, QueryValueEx, HKEY_CURRENT_USER, SetValueEx, REG_SZ, CloseKey, OpenKeyEx
 from subprocess import Popen
 from ctypes import windll
@@ -19,6 +18,7 @@ class Account(object):
         self.steamID64 = steamID64
         self.name_account = name_account
         self.name_persona = name_persona
+        self.time_stamp = time_stamp
         self.time_last = strftime('%Y-%m-%d %H:%M', localtime(time_stamp))
 
 
@@ -48,9 +48,7 @@ class Window(QWidget):
         self.getComboBox()  # 下拉框内容
         self.changeComboBox()  # 改变label显示内容
         # 定义组件的信号/槽
-        self.pushButton.clicked.connect(self.killSteam)  # 杀死steam进程
         self.pushButton.clicked.connect(self.loginSteam)  # 打开steam进程
-        self.pushButton.clicked.connect(self.quitMain)  # 关闭进程
         self.pushButton_config.clicked.connect(self.getUserConfig)  # 打开设置
         self.comboBox.currentIndexChanged.connect(self.changeComboBox)  # 改变label显示内容
         self.comboBox.currentTextChanged.connect(self.inputComboBox)  # 输入文本
@@ -74,7 +72,6 @@ class Window(QWidget):
 
     # 关闭主程序
     def quitMain(self):
-        sleep(0.5)
         quit()
 
     # 输入内容
@@ -90,17 +87,21 @@ class Window(QWidget):
 
     # 获取下拉框内容
     def getComboBox(self):
-        comboBoxItems = []
-        autoUser = ""
+        comboBoxItems = []  # 创建列表用来储存combobox的内容
+        autoUser = ""  # 创建临时变量储存目前登录的账号
         for i in self.accounts:
-            if i.name_account == self.user:
-                autoUser = i.name_account
+            if i.name_account == self.user:  # 判断如果为目前的登录账号，就赋值给autoUser
+                autoUser = i
             else:
-                comboBoxItems.append(i.name_account)
-        comboBoxItems.sort(key=lambda a: len(a), reverse=False)
-        if autoUser:
-            comboBoxItems.insert(0, autoUser)
-        self.comboBox.addItems(comboBoxItems)  # 添加到下拉框
+                comboBoxItems.append(i)  # 否则就添加到列表
+        comboBoxItems.sort(key=lambda account: account.time_stamp, reverse=True)  # 给列表按account的time_stamp排序
+        if type(autoUser).__name__ == 'Account':  # 如果autoUser赋值了Account对象
+            comboBoxItems.insert(0, autoUser)  # 就把autoUser插队到列表首位
+        for i in comboBoxItems:  # 循环列表
+            self.comboBox.addItem(i.name_account)  # 添加到下拉框
+            num = comboBoxItems.index(i)  # 获取元素在列表中的索引
+            tooltip = str(i.name_persona + '\n' + i.time_last)
+            self.comboBox.setItemData(num, tooltip, Qt.ToolTipRole)  # 根据索引添加tooltip
 
     # 改变label显示内容
     def changeComboBox(self):
@@ -119,16 +120,14 @@ class Window(QWidget):
         self.user = QueryValueEx(key, "AutoLoginUser")[0]
         CloseKey(key)
 
-    # 杀掉steam进程
-    def killSteam(self):
-        system('killSteam.bat')
-
     # 登录
     def loginSteam(self):
         key = OpenKeyEx(HKEY_CURRENT_USER, r'SOFTWARE\Valve\Steam', access=KEY_ALL_ACCESS)
         SetValueEx(key, "AutoLoginUser", 0, REG_SZ, self.comboBox.currentText())
         CloseKey(key)
+        system('killSteam.bat')  # 关闭后台的steam
         Popen(r"{}".format(self.exe_path))  # 打开steam
+        self.quitMain()
 
     # 获取vdf里面的账号信息
     def getAccount(self):
@@ -157,20 +156,37 @@ class Window(QWidget):
                 self.accounts.append(account)  # 列表保存刚刚实例化的对象
 
 
-# 创建qt对象
-app = QApplication(argv)
-# 创建页面对象加载ui文件
-window = Window()
-# 判断是否管理员模式运行
-if not windll.shell32.IsUserAnAdmin():
-    error = QMessageBox(QMessageBox.Critical, ' 请以管理员模式运行！！！！', '1、右键exe文件(或快捷方式)\n2、属性\n3、兼容性\n4、√以管理员身份运行此程序')
-    error.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowStaysOnTopHint)
-    error.setWindowIcon(QIcon('steam.ico'))
-    error.exec_()
-else:
-    # 展示窗口
-    window.ui.show()
-    # 程序循环等待操作
-    app.exec_()
+def main():
+    # 创建qt对象
+    app = QApplication(argv)
+    # 创建页面对象加载ui文件
+    window = Window()
+    # 判断是否管理员模式运行
+    if not windll.shell32.IsUserAnAdmin():
+        error = QMessageBox(QMessageBox.Critical, ' 请以管理员模式运行！！！！', '1、右键exe文件(或快捷方式)\n2、属性\n3、兼容性\n4、√以管理员身份运行此程序')
+        error.setWindowFlags(Qt.WindowCloseButtonHint | Qt.WindowStaysOnTopHint)
+        error.setWindowIcon(QIcon('steam.ico'))
+        error.exec_()
+    else:
+        # 展示窗口
+        window.ui.show()
+        # 设置标题
+        window.ui.setWindowTitle('Steam账号切换器_v1.1.2')
+        # 程序循环等待操作
+        app.exec_()
 
+
+# if __name__ == '__main__':
+#     # 创建qt对象
+#     app = QApplication(argv)
+#     # 创建页面对象加载ui文件
+#     window = Window()
+#     window.ui.setWindowTitle('开发者模式！！！测试使用！！！')
+#     # 展示窗口
+#     window.ui.show()
+#     # 程序循环等待操作
+#     app.exec_()
 # pyinstaller -F -w -i steam.ico --clean --version-file version_info.txt Main.py
+
+
+main()
